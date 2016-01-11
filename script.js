6019f72aa1725824b1a5ad39d59c657a05cc8c0e
Falk Schwiefert <falk.schwiefert@gmail.com>
2016-01-11 11:44:45 -0800

    added message when no events are found near search zip

--- script.js
+++ script.js
@@ -19,63 +19,73 @@ $('.button').click(function(){
 		dataType: "jsonp",
 		success: function(data){
 			console.log(data);
-		var i = 0;	
-		
-		$.each(data.results, function(){
-
-			//create the event wrapper
-			$('.wrapper').append('<div class="event' + ' ' + i + '"></div>')	
-
-			//select event wrapper and append content for each event
-
-			//check if content exists in json file, if it does append it, otherwise move on to next step
-			if (data.results[i].name){
-				$('.' +i).append('<div class="title"><h3>' + data.results[i].name + '</h3></div>' );
-			};
-			if (data.results[i].description) {
-				$('.' +i).append('<div class="description"><span>Click this field to read the full event description...</span></br>' + (data.results[i].description) + '...' + '' + '</div>');
-			};
-			if (data.results[i].time) {
-				$('.' +i).append('<div class="date">' + new Date(data.results[i].time) + '</div>');
-			};
-			if (data.results[i].venue) {
-				if ((data.results[i].venue.name) && (data.results[i].venue.address_1)) {
-					$('.' +i).append('<div class="venue">' + data.results[i].venue.name + ' @ ' + data.results[i].venue.address_1 + '</div>');
-				} else if (data.results[i].venue.name) {
-					$('.' +i).append('<div class="venue">' + data.results[i].venue.name + '</div>');
-				} else if (data.results[i].venue.address_1) {
-					$('.' +i).append('<div class="venue">' + data.results[i].venue.address_1 + '</div>');
-				} 
-			};
-			if (data.results[i].yes_rsvp_count) {
-				$('.' +i).append('<div class="rsvp_yes rsvp">Yes: ' + data.results[i].yes_rsvp_count + '</div>');
-			};
-			//if there is a no_rsvp_count then (if the object has a value of 0 it still needs to be manually set to 0 otherwise it is
-			//interpreted as boolean "false")
-			if (data.results[i].no_rsvp_count) {
-				//add this as a div
-				$('.' +i).append('<div class="rsvp_no rsvp">No: ' + data.results[i].no_rsvp_count + '</div>');
-				//otherwise
+			var i = 0;	
+			
+			//if there is no result for the search 
+			if (data.results.length < 1) {
+				$('.nothingHere').remove();
+				$('.wrapper').append('<div class="nothingHere">It seems like there are no upcoming tech meetups within 25 miles of zip code: ' + zipSearch + '</div>');
+			
+			//otherwise
 			} else {
-				//just write 0 
-				$('.' +i).append('<div class="rsvp_no rsvp">No: 0</div>');
-			};
-			if (data.results[i].maybe_rsvp_count) {
-				$('.' +i).append('<div class="rsvp_maybe rsvp">Maybe: ' + data.results[i].maybe_rsvp_count + '</div>');
-			} else {
-				$('.' +i).append('<div class="waitlist rsvp">Waitlist: 0</div>');
+				$('.nothingHere').remove();
+
+				$.each(data.results, function(){
+
+					//create the event wrapper
+					$('.wrapper').append('<div class="event' + ' ' + i + '"></div>')	
+
+					//select event wrapper and append content for each event
+
+					//check if content exists in json file, if it does append it, otherwise move on to next step
+					if (data.results[i].name){
+						$('.' +i).append('<div class="title"><h3>' + data.results[i].name + '</h3></div>' );
+					};
+					if (data.results[i].description) {
+						$('.' +i).append('<div class="description"><span>Click this field to read the full event description...</span></br>' + (data.results[i].description) + '...' + '' + '</div>');
+					};
+					if (data.results[i].time) {
+						$('.' +i).append('<div class="date">' + new Date(data.results[i].time) + '</div>');
+					};
+					if (data.results[i].venue) {
+						if ((data.results[i].venue.name) && (data.results[i].venue.address_1)) {
+							$('.' +i).append('<div class="venue">' + data.results[i].venue.name + ' @ ' + data.results[i].venue.address_1 + '</div>');
+						} else if (data.results[i].venue.name) {
+							$('.' +i).append('<div class="venue">' + data.results[i].venue.name + '</div>');
+						} else if (data.results[i].venue.address_1) {
+							$('.' +i).append('<div class="venue">' + data.results[i].venue.address_1 + '</div>');
+						} 
+					};
+					if (data.results[i].yes_rsvp_count) {
+						$('.' +i).append('<div class="rsvp_yes rsvp">Yes: ' + data.results[i].yes_rsvp_count + '</div>');
+					};
+					//if there is a no_rsvp_count then (if the object has a value of 0 it still needs to be manually set to 0 otherwise it is
+					//interpreted as boolean "false")
+					if (data.results[i].no_rsvp_count) {
+						//add this as a div
+						$('.' +i).append('<div class="rsvp_no rsvp">No: ' + data.results[i].no_rsvp_count + '</div>');
+						//otherwise
+					} else {
+						//just write 0 
+						$('.' +i).append('<div class="rsvp_no rsvp">No: 0</div>');
+					};
+					if (data.results[i].maybe_rsvp_count) {
+						$('.' +i).append('<div class="rsvp_maybe rsvp">Maybe: ' + data.results[i].maybe_rsvp_count + '</div>');
+					} else {
+						$('.' +i).append('<div class="waitlist rsvp">Waitlist: 0</div>');
+					}
+					if (data.results[i].waitlist_count) {
+						$('.' +i).append('<div class="waitlist rsvp">Waitlist: ' + data.results[i].waitlist_count + '</div>');
+					} else {
+						$('.' +i).append('<div class="waitlist rsvp">Waitlist: 0</div>');
+					};
+					if (data.results[i].event_url) {
+						$('.' +i).append('<div class="signup"><a href="' + data.results[i].event_url + '" target="blank">Sign up for the event</a></div>')
+					};
+					i++;
+				});
 			}
-			if (data.results[i].waitlist_count) {
-				$('.' +i).append('<div class="waitlist rsvp">Waitlist: ' + data.results[i].waitlist_count + '</div>');
-			} else {
-				$('.' +i).append('<div class="waitlist rsvp">Waitlist: 0</div>');
-			};
-			if (data.results[i].event_url) {
-				$('.' +i).append('<div class="signup"><a href="' + data.results[i].event_url + '" target="blank">Sign up for the event</a></div>')
-			};
-			i++;
-		});
-		}
+		}	
 	});
 	}
 });
